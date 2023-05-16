4338dea7b2eee818b4a07548f5cb9c0d40bf50c5
mk8877 <113814695+mk8877@users.noreply.github.com>
2023-05-16 20:16:49 +0900

    sheetの更新，ファイルのdir作成

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from flask import Flask
-from views import spreadsheet_view
+from views import spreadsheet_view, update_spreadsheet
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return spreadsheet_view()
+
+@app.route('/update', methods=['POST'])
+def update():
+    return update_spreadsheet()
 
 if __name__ == '__main__':
     app.run(debug=True)
